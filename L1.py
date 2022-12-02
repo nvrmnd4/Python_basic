eef1d1b22d544ac891da9f0bd1bfda1ae729d03e
nvrmnd4 <nvrmnd4u@gmail.com>
2022-12-02 20:17:10 +0200

    1 lection

---
+++ L1.py
@@ -0,0 +1,13 @@
+# Лекция 1 30.11.2022
+
+
+print('Hello!')
+print('What is your name?')
+name = input()
+print(f'Hello {name} !\nHave a nice day!')
+
+# динамическая типизация
+
+name = 5
+print("Hello", name)
+print(f"Hello, {name}!")
