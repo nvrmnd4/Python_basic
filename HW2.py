d9792c02291d0849f1bcfd907d9a814a027d1d83
nvrmnd4 <nvrmnd4u@gmail.com>
2022-12-12 21:52:30 +0200

    исправлена

--- HW2.py
+++ HW2.py
@@ -2,16 +2,16 @@
 
 # Задание №1
 
-word_input = input()
+word_input = str(input(""" Введите строку
+>> """))
 # перевести всё в нижний регистр
 word_low = word_input.lower()
 # убираем переход на новую строку
 word = word_low.replace("\n","")
-# убираем лишние знаки и перенос строки
+# убираем лишние знаки
 filtered_word = word.replace(',', '').replace('.', '').replace('?', '').replace(';', '').replace('!', '').replace(':', '').replace('-', '').replace("'", '').replace('"', '').replace(' ', '')
 # проверяем будет ли паллиндромом
 if str(word) == str(word)[::-1] :
     print(f"Вы ввели {filtered_word}, это паллиндром")
 else:
     print(f"Вы ввели {filtered_word}, это не является паллиндромом")
-
