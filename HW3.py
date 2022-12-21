57b09067a4f87fa7b61765dad63d6635d2145622
nvrmnd4 <nvrmnd4u@gmail.com>
2022-12-21 14:59:18 +0200

    Доделала ввод и чтобы буквы были все одинаковые

--- HW3.py
+++ HW3.py
@@ -1,11 +1,20 @@
 # Домашнее задание №3 Полищук Анастасия
 
-user_input_1 = str(input())
 while True:
     try:
-        if user_input_1.lower() == 'Привіт':
-            print('Доброго вечора, я бот з України!')
-        elif user_input_1.lower() == 'Хай':
-            print('Доброго вечора, я бот з України!')
-        elif user_input_1.lower() == 'Доброго вечора':
-            print('Доброго вечора, я бот з України!')
+        inp = str(input())
+        inp = inp.capitalize()
+        if 'Привіт' in inp or "Хай" in inp or "Доброго вечора" in inp:
+            print("Доброго вечора, я бот з України!")
+        elif "Як справи?" in inp or "Що робиш?" in inp or "Чим займаєшся?" in inp:
+            print ("Вчусь програмувати на Python!")
+        elif "Фільм" in inp or "Кінотеатр" in inp or "Серіал" in inp:
+            print("Соррі що втручуюсь, не знаю про що йдеться мова, але подивіться фільм Бойцовський клуб, він просто бомба!")
+        elif "Бувай" in inp or "Надобраніч" in inp or "Гудбай" in inp or "До зустрічі" in inp:
+            print("Побачимось у мережі, I'll be back")
+        else:
+            print("Дуже цікаво, але, нажаль, нічого не зрозуміло :(")
+            break
+    except Exception:
+        print("Дуже цікаво, але, нажаль, нічого не зрозуміло :(")
+        continue
