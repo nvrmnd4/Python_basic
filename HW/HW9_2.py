ad1d744dcbdbf48a8b690d43110e4fc8b4ce1802
nvrmnd4 <nvrmnd4u@gmail.com>
2023-01-25 19:54:13 +0200

    HW9_2

--- HW/HW9_2.py
+++ HW/HW9_2.py
@@ -1,1 +1,15 @@
 # ДОМАШНЕЕ ЗАДАНИЕ №9 Полищук Анастасия
+
+def word_gen(text: str):
+    """
+    Генератор
+    :param text: строка s
+    :return: при каждом запросе выдает следующее слово из строки s
+    """
+    s = text.split()
+    for i in s:
+        yield i
+
+if __name__ == '__main__':
+    for word in word_gen('i am generating words from text'):
+        print(word)
